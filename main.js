b043cc9f63092345e1d8da4f93c25608cb1856a5
Malik-Y <125848099+Malik-Y@users.noreply.github.com>
2023-03-24 00:11:42 -0400

    Update main.js
    initial commit

--- main.js
+++ main.js
@@ -1,44 +1,92 @@
-//
-//  JS File
-//  You may remove the code below - it's just boilerplate
-//
+window.addEventListener('load', () => {
+  const form = document.querySelector('#new-task-form');
+  const input = document.querySelector('#input-task');
+  const list_el = document.querySelector('#tasks');
+  const empty_state_el = document.querySelector('#empty-state');
 
-//
-// Variables
-//
 
-// Constants
-const appID = "app";
-const headingText = "To do. To done. ✅";
+  empty_state_el.style.display = 'block';
 
-// Variables
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
+    const task = input.value;
 
-//
-// Functions
-//
+    if (!task) {
+      alert('Please Enter Your Task');
+      return;
+    }
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
+    const task_el = document.createElement('div');
+    task_el.classList.add('task');
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
+    const task_content_el = document.createElement('div');
+    task_content_el.classList.add('content');
 
-  // Init complete
-  console.log("App successfully initialised");
-}
+    
+    empty_state_el.style.display = 'none';
 
-//
-// Inits & Event Listeners
-//
+    task_el.appendChild(task_content_el);
 
-inititialise();
+    const task_input_el = document.createElement('input');
+    task_input_el.classList.add('text');
+    task_input_el.type = 'text';
+    task_input_el.value = task;
+    task_input_el.setAttribute('readonly', 'readonly');
+
+    task_content_el.appendChild(task_input_el);
+
+    const task_actions_el = document.createElement('div');
+    task_actions_el.classList.add('actions');
+
+    const task_edit_el = document.createElement('button');
+    task_edit_el.classList.add('edit');
+    task_edit_el.innerHTML = 'Edit';
+
+    const task_delete_el = document.createElement('button');
+    task_delete_el.classList.add('delete');
+    task_delete_el.innerHTML = 'Delete';
+
+    const task_done_el = document.createElement('button');
+    task_done_el.classList.add('done');
+    task_done_el.innerHTML = 'Done';
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
+    task_actions_el.appendChild(task_done_el);
+
+    task_el.appendChild(task_actions_el);
+    list_el.appendChild(task_el);
+
+    input.value = '';
+
+    if (list_el.children.length === 1) {
+      empty_state_el.style.display = 'none';
+    }
+
+    task_edit_el.addEventListener('click', () => {
+      if (task_edit_el.innerText.toLowerCase() == 'edit') {
+        task_input_el.removeAttribute('readonly');
+        task_input_el.focus();
+        task_edit_el.innerText = 'Save';
+      } else {
+        task_input_el.setAttribute('readonly', 'readonly');
+        task_edit_el.innerText = 'Edit';
+      }
+    });
+
+    task_delete_el.addEventListener('click', () => {
+      list_el.removeChild(task_el);
+     
+      if (list_el.children.length === 0) {
+        empty_state_el.style.display = 'block';
+      }
+    });
+
+    task_done_el.addEventListener('click', () => {
+      task_el.classList.toggle('completed');
+    });
+
+    localStorage.setItem('TaskList', JSON.stringify(list_el.innerHTML));
+  });
+});
